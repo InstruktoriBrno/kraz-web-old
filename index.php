2d59ede67f76678ad536c7831bc2b701b045482c
Martin Ukrop <mukrop@mail.muni.cz>
2020-03-03 22:59:58 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>KRAZ - outdoorový týmový závod</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <!-- Theme CSS -->
+    <link href="css/grayscale.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <i class="fa fa-play-circle"></i> <span class="ignis">KRAZ</span>
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">Úvod</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#facts">Informace</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#rules">Pravidla</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#signup">Přihlášení</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#team">Tým</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Kontakt</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">KRAZ</h1>
+                        <p class="intro-text">Outdoorový týmový závod
+                            <br/> 12. - 13. 11. 2016
+                            <br/> Moravský kras</p>
+														<a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section id="about" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Outdoorový závod KRAZ</h2>
+                <p class="italic">Hluboké lesy Moravského krasu čekají. Co ještě zvládneš a&nbsp;co už je nad tvoje síly? A&nbsp;máš se o&nbsp;koho opřít?</p>
+                <p>KRAZ je outdoorový závod s&nbsp;několikaletou tradicí, pořádaný sdružením <a href="http://www.halahoj.org/">Halahoj</a> a nově i pod&nbsp;hlavičkou <a href="http://www.instruktori.cz/">
+                   Instruktorů Brno</a>. Čtyřčlenné týmy překonávají trasu náročným terénem a hledají stanoviště, která jim prozradí další úsek cesty.
+                   O&nbsp;celkovém výsledku rozhoduje čas dokončení závodu. 
+                   Závod začíná v sobotu odpoledne a končí v neděli, počítejte, že se přes noc moc nevyspíte.
+                </p>
+                <p>Závod se tradičně koná v&nbsp;oblasti Moravského krasu severně od&nbsp;Brna. Trať není předem známá, týmy se&nbsp;samy orientují podle mapy nebo jiných vodítek, která najdou na stanovišti.
+                Řádově se&nbsp;jedná o&nbsp;desítky (cca&nbsp;20-40) kilometrů. Cestou vás může potkat prakticky cokoli, od neúplných map přes outdoorové aktivity až po rébusy a hádanky.
+                </p>
+                <p> Pro lepší představu se&nbsp;podívejte na&nbsp;web <a href="http://zavodkraz.cz/stary" target="_blank">minulých ročníků</a>.</p>
+                <h3>Na co se můžete těšit</h3>
+                <p>Krásnou příroda, nečekané úkoly, krev, dřinu, pot a slzy.</p>
+                <h3>Čeho se nemusíte bát</h3>
+                <p>Deště, zimy, smrti z vyčerpání a posměšků od soupeřů.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Facts Section -->
+    <section id="facts" class="content-section text-center">
+        <div class="facts-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Základní informace</h2>
+<h3>Termín a místo</h3>
+<p>Víkend 12.&nbsp;-&nbsp;13.&nbsp;11. 2016 v Moravském krasu (místo startu bude upřesněno).</p>
+<h3>Účastníci</h3>
+<p>Akce je učena pro tříčlenné nebo čtyřčlenné týmy (15 a více let). Na výkonnosti ani pohlaví nezáleží; na své si přijdou namakaní borci, rekreační sportovci i občasní dobrodruhové.</p>
+<h3>Cena</h3>
+<p>Cena akce je 850,- Kč/tým. Zahrnuje tisk map, zajištění materiálu, pronájem základny a občerstvení v cíli.</p>
+<h3>Storno podmínky</h3>
+<p>Storno poplatky činí 30&nbsp;% (odhlášení více než 30 dní před započetím akce), 50&nbsp;% (30 až 10 dní), 90&nbsp;% (9 až 1 den předem) či 100&nbsp;% (v&nbsp;den započetí akce či později). 
+V případě zrušení akce ze strany organizátorů vracíme 100&nbsp;% uhrazené částky. Je možné měnit složení týmu (= najít za sebe náhradu).</p>
+<h3>Přihlašování</h3>
+<p>Přihlašování bude spuštěno 10. října 2016 v 20:20. Kapacita závodu je stanovena na 50 týmů. Proto neváhejte a přihašte se co nejdříve. Týmy, kteří se účastnili KRAZu na jaře dostanou přednostní kód pro přihlašování, první tři týmy z jara mají startovné zdarma.</p>
+
+<h3>Dotazy</h3>
+<p>Případné dotazy směřujte buď na <a class="link" href="https://www.facebook.com/events/179944522443155/" target="_blank" title="KRAZ na Facebooku">Facebook</a> (pozvěte i svoje kamarády) nebo na&nbsp;e-mailovou adresu
+<a href="mailto:zavodkraz@gmail.com">zavodkraz@gmail.com</a>.
+</div>
+</div>
+</div>
+</section>
+
+    <!-- Rules Section -->
+    <section id="rules" class="content-section text-center">
+        <div class="rules-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Pravidla</h2>
+                    <p><strong>POZOR</strong>: Níže uvedná pravidla jsou pouze informační a mohou se měnit. 
+                    Konečnou verzi pošleme všem přihlášným e-mailem týden před hrou.
+                    </p>
+
+<h3>Start a průběh závodu - podzim 2016:</h3> 
+
+<ul class="tleft">
+<li>Cílem závodu je projít všemi kontrolními stanovišti v&nbsp;daném pořadí 
+(není-li určeno jinak) až do cíle, jehož umístění není předem známo. Volba trasy mezi stanovišti je na vašem uvážení.</li>
+<li>Maximální počet týmů je 50 + 5 později vybraných na divokou kartu.</li>
+<li>Po cestě není nutné plnit žádné další úkoly na stanovištích ani žádné šifry. Závod je zajímavý zejména svými úseky a atmosférou denního a nočního lesa.</li>
+<li>Závodí výhradně týmy o <strong>3&ndash;4 lidech</strong>, všichni členové týmu musí být <strong>starší patnácti let</strong>. Pokud chtějí soutěžit mladší účastníci, kontaktujte nás soukromě.</li> 
+<li>Registrace týmů bude spuštěna 10.10. ve 20:20. Start závodu je<strong> v sobotu 12. listopadu 2016 odpoledne</strong>. </li> 
+<li><strong>Místo startu</strong> bude zasláno výhradně registrovaným týmům 3 dny před začátkem závodu. Místo se nachází v bezprostřední blízkosti Brna v dosahu IDS&nbsp;JMK</li>
+<li>Polohu stanoviště se dozvíte vždy na předchozím bodě. Od okamžiku startu máte <strong>20&nbsp;hodin</strong></li>
+</ul>
+<h3>V cíli</h3>
+<p>Cíl závodu bude na místě s s&nbsp;možností občerstvení (v&nbsp;ceně), přespáním, sprchou atd.</p>
+<p>Vyhlášení výsledků proběhne, jakmile bude jasné pořadí na prvních třech místech.</p> 
+
+<h3>Povinná výbava</h3>
+
+ 
+<ul>
+<li>Buzola nebo kompas</li>
+<li>Mobilní telefon</li>
+<li>Digitální fotoaparát</li>
+<li>GPS navigace</li>
+
+<p>Tyto předměty musí být oddělené, není tedy možné mít jediný telefon, na kterém bude i&nbsp;buzola a kompas, celkem tedy musíte mít přinejmenším čtyři různé předměty.</p>
+</ul>
+
+<h3>Doporučená výbava</h3>
+<ul>
+<li>Čelovka pro každého člena týmu</li>
+<li>Silná baterka</li>
+<li>Náhradní baterie</li>
+<li>Náhradní baterie</li>
+<li>Náhradní baterie</li>
+</ul>
+
+<p>Další doporučené vybavení: náhradní boty a oblečení s ohledem na aktuální počasí a teplotu. Dostatek kalorií a tekutin na dvacet hodin. Závod je dlouhý a vede náročným terénem. Spacák a karimatka nejsou během závodu nutné (v rozumné míře zajistíme převoz věcí ze startu do cíle).</p>
+
+
+<h3>Zakázané vybavení:</h3>
+<p><strong>Je přísně zakázáno používat při závodě vlastní mapy.</strong> Smíte využít jen těch map, které dostanete od nás. Máte-li mapu v&nbsp;mobilu či GPSce, musí být možné ji vypnout, jinak nebude možné takovou navigaci pro závod uznat.</p> 
+
+<p>Během závodu je zakázano k přesunům používat motorové prostředky, kola, koloběžky, koně, osly, slony a jiné prostředky mimo vlastní nohy.</p> 
+
+<h3>Prostor závodu:</h3>
+<p>Kopcovitý, lesnatý terén různého charakteru. Během závodu se budete pohybovat prakticky jen v lese.</p> 
+
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    
+    <!-- Signup Section -->
+    <section id="signup" class="content-section text-center">
+        <div class="signup-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Přihlašování</h2>
+                  
+ 
+ <form>
+  <div class="form-group col-xs-7">
+    <input type="text" class="form-control" id="team_name" placeholder="Název týmu">
+  </div>
+  <div class="form-group col-xs-7">
+    <input type="email" class="form-control" id="email" placeholder="Kontaktní email">
+  </div>
+  <div class="form-group col-xs-7">
+    <input type="text" class="form-control" id="phone"  placeholder="Kontaktní telefon">
+  </div>
+  <div class="form-group col-xs-7">
+    <input type="password" class="form-control" id="password"  placeholder="Heslo">
+  </div>
+  <div class='clearfix'></div>
+  <h3>Složení týmu</h3>
+  <div class="form-group clearfix">
+   <div class="col-xs-5">
+     <input type="text" class="form-control" id="person1" placeholder="1. člen">
+   </div>
+   <div class="col-xs-2">
+     <input type="text" class="form-control" id="person1age" placeholder="věk">
+   </div>
+  </div>
+  
+  <div class="form-group clearfix">
+   <div class="col-xs-5">
+     <input type="text" class="form-control" id="person2" placeholder="2. člen">
+   </div>
+   <div class="col-xs-2">
+     <input type="text" class="form-control" id="person2age" placeholder="věk">
+   </div>
+  </div>
+  
+  <div class="form-group clearfix">
+   <div class="col-xs-5">
+     <input type="text" class="form-control" id="person3" placeholder="3. člen">
+   </div>
+   <div class="col-xs-2">
+     <input type="text" class="form-control" id="person3age" placeholder="věk">
+   </div>
+  </div>
+  
+  <div class="form-group clearfix">
+   <div class="col-xs-5">
+     <input type="text" class="form-control" id="person4" placeholder="4. člen">
+   </div>
+   <div class="col-xs-2">
+     <input type="text" class="form-control" id="person4age" placeholder="věk">
+   </div>
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</div>
+</div>
+</div>
+</section>
+    
+    <!-- Team Section -->
+    <section id="team" class="content-section text-center">
+        <div class="team-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Organizátorský tým</h2>
+                    <div class="team-left team">
+                        <img class="organizer left" src="img/tym/michal.jpg" />
+                        <h4>Michal</h4>
+                        <p>Zkušený organizátor a účastník outdoorových závodů, orienťák, občanským povoláním advokát. Ale nečekejte, že vás během závodu budu vysekávat z problémů, do kterých se dostanete. Naopak, nechám vás v tom maximálně vymáchat. </p>
+                        <div class="cleaner">&nbsp;</div>
+                    </div>
+                    <div class="team-right team">
+                        <img class="organizer right" src="img/tym/hani.jpg" />
+                        <h4>Hanka</h4>
+                        <p>Nemám vlastně žádný orientační smysl, s mapou se ztratím naprosto kdekoliv a buzola to jen zhorší. V týmu plném orienťáků jsem vlastně omylem. Ale miluji výzvy a KRAZ patří mezi ně.</p>
+                        <div class="cleaner">&nbsp;</div>
+                    </div>
+                    <div class="team-left team">
+                        <img class="organizer left" src="img/tym/honza.jpg" />
+                        <h4>Honza</h4>
+                        <p>Orienťák, vodní skaut, účastník a organizátor šifrovacích her. Jen tak, abyste měli představu, co vás čeká.</p>
+                        <div class="cleaner">&nbsp;</div>
+                    </div>
+                    <div class="team-right team">
+                        <img class="organizer right" id="glum" src="img/tym/glum2.jpg" onmouseover="this.src='img/tym/glum.jpg';" onmouseout="this.src='img/tym/glum2.jpg';"/>
+                        <h4>Glum</h4>
+                        <p>Nezničitelný dříč, který překonal Krkonoše, Tatry, Pyreneje, Alpy i Kavkaz. Nexistuje kopec, který by nezvládl zdolat, les, který by nedokázal zmapovat, ani řeka, kterou by se neodvážil splout.
+                        <div class="cleaner">&nbsp;</div>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    
+    
+    <section class="content-section text-center">
+        <div class="contact-section">
+            <div class="container">
+                <div id="contact" class="col-lg-8 col-lg-offset-2">
+                    <h2>Kontakt</h2>
+                    <a href="http://www.instruktori.cz/"><img id="footer-sova" src="img/footer-sova.png" /></a>
+                    <p>KRAZ je tradiční outdoorový závody pořádaný <a href="http://halahoj.org" target="_blank">spolkem Halahoj</a> a <a href="http://www.instruktori.cz/">Instruktory Brno</a>.</p>
+                    <p>Jakékoliv dotazy ohledně akce nám pište emailem přímo na adresu <a href="mailto:zavodkraz@gmail.com">zavodkraz@gmail.com</a></p>
+                </div>
+                <div id="footer" class="col-lg-8 col-lg-offset-2">
+                    <p>Webdesign Honza Klusáček 2016, založeno na motivu <a href="http://startbootstrap.com/template-overviews/grayscale/">Grayscale</a>, 
+                    autor obrázků: Martin Vágner (KRAZ 2013), týmy „Ztraceni a opilí“ a „Tři“ (KRAZ 2014).</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/grayscale.min.js"></script>
+
+</body>
+
+</html>
